467fd8e35dcf793684a07a0cc3d06492168b72fb
Nikita Zverev <nzverev@appliedtech.ru>
2018-12-16 12:59:09 +0500

    Added simple filters to the projects component

--- src/app/projects/projects/projects.component.ts
+++ src/app/projects/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectService } from 'src/app/projects/services/project.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
@@ -9,16 +10,22 @@ import { ProjectService } from 'src/app/projects/services/project.service';
 export class ProjectsComponent implements OnInit {
 
   projects: any[] = [];
+  languages: any[] = [];
 
   constructor(
-    private projectService: ProjectService
+    private projectService: ProjectService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.projectService.getProjects().subscribe(res => {
-      this.projects = res;
-      console.log(this.projects);
+      this.projects = res.repos;
+      this.languages = res.languages;
+      console.log(this.projects, this.languages);
     });
   }
-
+  projectClick(project: any) {
+    const url = `/projects/${project.owner.login}/${project.name}/branches`;
+    this.router.navigateByUrl(url);
+  }
 }
