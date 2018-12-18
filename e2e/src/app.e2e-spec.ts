26106ee1815242d8094661cf8aa56eb02c703e6a
Nikita Zverev <nikita.zverev@konverso.ai>
2018-12-18 16:05:58 +0500

    Added simple e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser, ExpectedConditions, by } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -7,8 +8,18 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to github-projects!');
+  it('should display a search field for organizations', () => {
+    page.navigateTo('/');
+    const searchField = page.getElementByClass('projects__organization');
+    expect(searchField).toBeTruthy();
+  });
+
+  it('should find projects and dispaly then as a list', () => {
+    page.navigateTo('/projects?owner=payworks');
+    const projectsContent = page.getElementByClass('projects__content');
+    expect(projectsContent).toBeTruthy();
+
+    const projectListItems = page.getElementByClass('projects-list');
+    expect(projectListItems).toBeTruthy();
   });
 });
